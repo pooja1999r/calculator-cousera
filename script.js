06426861df10077dadc0d67e7c86a552db40a074
pooja1999r <84758687+pooja1999r@users.noreply.github.com>
2021-12-25 14:46:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function compute()
     document.getElementById("rate_val").innerText=reteval+"%.";
    var rate = document.getElementById("rate").value;
    var years = document.getElementById("years").value;
-   var interest =(parseInt( principal)*parseInt( years)*parseInt( rate))/100+principal;
+   var interest =(parseInt( principal)*parseInt( years)*parseInt( rate))/100;
    var year =new Date().getFullYear()+parseInt(years);
    document.getElementById("result").innerHTML ="if your deposit "+ principal+"<br>at an interest rate of"+rate+ "%." + "<br>You will receive an amount of" +interest+"," +"<br>in the year"+ year+"<br>";
    }
